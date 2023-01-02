4f033b5d6144e9dc1d579c114f11929c2877622d
sseong30 <hoi3330@gmail.com>
2023-01-02 21:38:13 +0900

    project section 영역 upload

---
+++ js/common.js
@@ -0,0 +1,19 @@
+// modal bg
+$(function() {
+    $(".project_list .list").on('click', function(){
+        $(".pop_bg").addClass('show');
+    });
+    $(".pop .close_btn,.pop_bg").on('click', function(){
+        $(".pop,.pop_bg").removeClass('show');
+    });
+});
+
+// modal
+function showPop(modalname) {
+    var $layer = $("."+ modalname);
+    $layer.addClass("show");
+}
+
+function hidePop(modalname){
+    $("."+ modalname).removeClass("show");
+}
