bca2c6a49e59bc812e51f11e87a814eab26ae1d7
sseong30 <hoi3330@gmail.com>
2023-01-03 01:47:58 +0900

    section title 추가

--- js/common.js
+++ js/common.js
@@ -17,3 +17,21 @@ function showPop(modalname) {
 function hidePop(modalname){
     $("."+ modalname).removeClass("show");
 }
+
+// scroll
+$(document).ready(function() {
+    //jbOffset에 .project_section의 좌표값 저장 
+	var jbOffset = $('.project_section').offset();
+
+	$(window).scroll(function() {
+        //document가 jbOffset 값보다 많이 스크롤 되면 실행
+		// if ( $(document).scrollTop() > jbOffset.top ) {
+        if (jbOffset.top >= 10) {
+			$('.project_section .section_title').addClass('on');
+		}
+        //jbOffset 값 이하로 줄어들면 실행
+		else {
+			$('.project_section .section_title').removeClass('on');
+		}
+	});
+});
